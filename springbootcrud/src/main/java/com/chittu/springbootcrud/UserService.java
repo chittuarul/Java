127dec4e2fb82e8c576619d7d5076caf08711056
chittuarul <chittuarul@gmail.com>
2023-05-08 15:47:29 +0530

    java spring boot  first commit

---
+++ springbootcrud/src/main/java/com/chittu/springbootcrud/UserService.java
@@ -0,0 +1,14 @@
+package com.chittu.springbootcrud;
+import java.util.List;
+
+public interface UserService {
+    User createUser(User user);
+
+    User getUserById(Long userId);
+
+    List<User> getAllUsers();
+
+    User updateUser(User user);
+
+    void deleteUser(Long userId);
+}
